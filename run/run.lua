13e4b84a738adcef24ec4eafb6f30ed02e09e290
luarocks <luarocks@onionmail.org>
2022-01-24 14:44:15 +0200

    Add crush logger

--- run/run.lua
+++ run/run.lua
@@ -1,3 +1,14 @@
 print("\n===================RUN==================\n")
 
-require("init")
+local succes, result = pcall(require, "init")
+
+if not succes then
+  result = tostring(result)
+
+  local log = require("lib.any.log")
+  log.outfile = "crash.log"
+  log.fatal(result)
+
+  local execute = require("lib.os.execute")
+  execute("notify-send \"" .. result .. "\"")
+end
