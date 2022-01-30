dbc97179a50a3b3bdbd843d2e7bb6adab5c4fdc2
luarocks <luarocks@onionmail.org>
2022-01-30 05:39:01 +0200

    Fix package.path for default luajit

--- run/run.lua
+++ run/run.lua
@@ -1,5 +1,8 @@
 print("\n===================RUN==================\n")
 
+package.path = "?/init.lua;" .. package.path
+package.cpath = "" .. package.cpath
+
 local succes, result = pcall(require, "init")
 
 if not succes then
